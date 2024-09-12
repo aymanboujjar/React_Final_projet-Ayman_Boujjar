c6d607dc8827e36d81da4abf8ffb85ca9f896053
ayman_bojo <aymenboujjar12@gmail.com>
2024-09-12 17:01:27 +0100

    secound caroussel

---
+++ src/pages/components/caroussel2.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Slider from "react-slick";
+import images from "../../constant/images";
+import data from "../../json/data.json";
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
+
+const PrevArrow = ({ onClick }) => {
+  return (
+    <button
+      className="absolute text-3xl left-2 top-1/2 transform -translate-y-1/2 z-10 text-black rounded-full p-2"
+      onClick={onClick}
+    >
+      <FaArrowCircleLeft />
+    </button>
+  );
+};
+
+const NextArrow = ({ onClick }) => {
+  return (
+    <button
+      className="absolute text-3xl right-2 top-1/2 transform -translate-y-1/2 z-10 text-black rounded-full p-2"
+      onClick={onClick}
+    >
+      <FaArrowCircleRight />
+    </button>
+  );
+};
+const Carousel2 = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4, 
+    slidesToScroll: 4,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+    responsive: [
+      
+      {
+        breakpoint: 768, 
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
+  return (
+    <>
+    <div className='mt-10 flex justify-center items-center'>
+      <h1 className='text-4xl font-extrabold mb-3'>FEATURED PRODUCTS</h1>
+    </div>
+    <div className="slider-container overflow-hidden ">
+      <Slider className='px-10' {...settings}>
+        {data.map((e, i) => (
+          <div key={i} className="p-2"> 
+            <img src={images[e.image]} alt={e.title} className="w-full h-auto" />
+            <h1>{e.title}</h1>
+            <p>{e.price}</p>
+          </div>
+        ))}
+      </Slider>
+    </div>
+    </>
+
+  );
+};
+
+export default Carousel2;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@
 import CAroussel from "./components/home/caroussel";
 import Navbar from "./components/home/navbar";
 import Cards from "./components/home/cards";
+import Carousel2 from "./components/caroussel2";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
     <Navbar/>
     <CAroussel/>
     <Cards/>
+    <Carousel2/>
     </>
   );
 }
