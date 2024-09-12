dcb94fee59948dc18b9e66e8f636c5a82d17b78b
ayman_bojo <aymenboujjar12@gmail.com>
2024-09-12 11:48:43 +0100

    first commit

---
+++ src/pages/components/hero/navbar.jsx
@@ -0,0 +1,32 @@
+import { CgProfile } from "react-icons/cg";
+import { SlBag } from "react-icons/sl";
+import { TbPointFilled } from "react-icons/tb";
+
+function Navbar() {
+  return (
+    <>
+      <div className="flex items-center justify-around mt-5">
+        <h1 className="text-4xl font-extrabold flex relative">
+          Fashe
+          <span className="text-red-600 absolute bottom-[0px] text-2xl right-[-20px]">
+            <TbPointFilled />
+          </span>
+        </h1>
+        <ul className="flex gap-x-8">
+          <li className="hover:underline hover:text-black ">Home</li>
+          <li className="hover:underline hover:text-black ">Shop</li>
+          <li className="hover:underline hover:text-black ">Sale</li>
+          <li className="hover:underline hover:text-black ">Blog</li>
+          <li className="hover:underline hover:text-black ">About</li>
+          <li className="hover:underline hover:text-black ">Contact</li>
+        </ul>
+        <div className="flex gap-x-4">
+          <CgProfile />
+          <SlBag />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/hero/navbar";
+
+
+function Home() {
+  return (
+    <Navbar/>
+  );
+}
+
+export default Home;
+
+
+
+
+
+
